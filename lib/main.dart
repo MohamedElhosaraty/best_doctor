507fede1423ca13e1ca4ab6020fa5bd6fe787e5a
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2024-08-29 20:42:16 +0300

    edit to pubspec.yaml and main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:best_doctor/core/di/dependency_injection.dart';
 import 'package:best_doctor/core/routing/app_router.dart';
 import 'package:best_doctor/doctor_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setupGetIt();
   runApp( DoctorApp(
     appRouter: AppRouter(),
   ));
