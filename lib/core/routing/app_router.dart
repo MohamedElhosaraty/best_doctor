6faf7f577464fcf3653ac92d48392c5d5413beb2
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2024-08-29 20:45:58 +0300

    work app_router and file_color

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,35 @@
+import 'package:best_doctor/core/di/dependency_injection.dart';
+import 'package:best_doctor/core/routing/routes.dart';
+import 'package:best_doctor/feature/login/logic/cubit/login_cubit.dart';
+import 'package:best_doctor/feature/login/ui/login_screen.dart';
+import 'package:best_doctor/feature/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppRouter {
+  Route generateRouter(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) => const OnboardingScreen(),
+        );
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (_) =>
+              BlocProvider(
+                create: (context) => getIt<LoginCubit>(),
+                child: const LoginScreen(),
+              ),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) =>
+              Scaffold(
+                body: Center(
+                  child: Text("No Route defined for ${settings.name}"),
+                ),
+              ),
+        );
+    }
+  }
+}
