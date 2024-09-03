02341ef609fbd0c63df47049ad67bc1a96b31756
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2024-09-03 22:35:32 +0300

    finish network

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:best_doctor/core/di/dependency_injection.dart';
 import 'package:best_doctor/core/routing/app_router.dart';
 import 'package:best_doctor/doctor_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
   setupGetIt();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
   runApp( DoctorApp(
     appRouter: AppRouter(),
   ));
 }
-
-// dart run build_runner build --delete-conflicting-outputs
